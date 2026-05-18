674bd87f16734dbbcdf546dd84bca1c92ad3055a
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-05-18 18:27:04 +0200

    sidebar sx pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import store from "./redux/store"
 import NavabarLinkedin from "./components/NavabarLinkedin"
+import SideBarSxPaginaHome from "./components/pagina home/SidebarSxPaginaHome"
 
 function App() {
   return (
     <Provider store={store}>
       <NavabarLinkedin />
+      <SideBarSxPaginaHome />
     </Provider>
   )
 }
---
+++ src/components/pagina home/SidebarSxPaginaHome.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+import { Col, Container, Row } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import getProfilePersonaleAction from "../../redux/actions/profiloPersonal"
+import Card1SidebarSx from "./card1SidebarSx"
+import Card2SidebarSx from "./Card2SidebarSx"
+import Card3SidebarSx from "./Card3Sidevasx"
+
+const SideBarSxPaginaHome = () => {
+  const profilo = useSelector((storeRedux) => {
+    return storeRedux.profile.me
+  })
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getProfilePersonaleAction())
+  }, [])
+
+  if (!profilo) return null
+  return (
+    <Container>
+      <Row>
+        <Col xs={2}>
+          <Card1SidebarSx profilo={profilo} />
+          <Card2SidebarSx />
+          <Card3SidebarSx />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+export default SideBarSxPaginaHome
