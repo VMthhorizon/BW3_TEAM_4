70f7a52b233b5cda80f9de047be7d2890ae7f57d
Vals098 <valeria.farinosi@gmail.com>
2026-05-18 14:31:44 +0200

    update: deleting to avoid conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import { Col, Container, Row } from "react-bootstrap"
 import "./App.css"
-import SidebarRight from "./components/SidebarRight/SidebarRight"
 
-
-function App() {
-  return (
-    <Container>
-      <Row>
-        <Col xs={3}>
-          LeftSidebar
-        </Col>
-
-        <Col xs={6}>
-          MainProfile
-        </Col>
-
-        <Col xs={3}>
-          <SidebarRight />
-        </Col>
-      </Row>
-    </Container>
-  )
-}
+function App() {}
 
 export default App
