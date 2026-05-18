c3932d7997e4fe5eb558118748870da72a9f0650
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-05-18 18:57:23 +0200

    implementaato router

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import store from "./redux/store"
 import NavabarLinkedin from "./components/NavabarLinkedin"
 import SideBarSxPaginaHome from "./components/pagina home/SidebarSxPaginaHome"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ProfilePage from "./components/pages/ProfilePage"
 
 function App() {
   return (
     <Provider store={store}>
-      <NavabarLinkedin />
-      <SideBarSxPaginaHome />
+      <BrowserRouter>
+        <NavabarLinkedin />
+        <Routes>
+          <Route path="/" element={<SideBarSxPaginaHome />} />
+          <Route path="/me" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   )
 }
