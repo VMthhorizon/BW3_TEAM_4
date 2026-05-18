e34f28938cc088b8b6070c7575a28607f50b2656
itsemanuela <notemanu152@gmail.com>
2026-05-18 12:08:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import store from "./redux/store";
 import NavabarLinkedin from "./components/NavabarLinkedin";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <Provider store={store}>
       <NavabarLinkedin />
+      <MyFooter />
     </Provider>
   );
 }
