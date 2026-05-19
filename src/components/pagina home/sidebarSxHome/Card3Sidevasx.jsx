a9c56cdcf7de20e0ea5338e5004effe1a9f98601
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-05-19 11:44:17 +0200

    fix

--- src/components/pagina home/sidebarSxHome/Card3Sidevasx.jsx
+++ src/components/pagina home/sidebarSxHome/Card3Sidevasx.jsx
@@ -5,20 +5,22 @@ const Card3SidebarSx = () => {
     <Card>
       <Card.Body>
         <Card.Text className="mb-0">
-          <i class="bi bi-bookmark-fill text-black"></i>
-          <p className="fw-bold d-inline-block ms-2 small">Elementi salvati</p>
+          <i className="bi bi-bookmark-fill text-black"></i>
+          <span className="fw-bold d-inline-block ms-2 small">
+            Elementi salvati
+          </span>
         </Card.Text>
         <Card.Text className="mb-0">
-          <i class="bi bi-people-fill text-black"></i>
-          <p className="fw-bold d-inline-block ms-2 small">Gruppi</p>
+          <i className="bi bi-people-fill text-black"></i>
+          <span className="fw-bold d-inline-block ms-2 small">Gruppi</span>
         </Card.Text>
         <Card.Text className="mb-0">
-          <i class="bi bi-newspaper text-black"></i>
-          <p className="fw-bold d-inline-block ms-2 small">Newsletter</p>
+          <i className="bi bi-newspaper text-black"></i>
+          <span className="fw-bold d-inline-block ms-2 small">Newsletter</span>
         </Card.Text>
         <Card.Text className="mb-0">
-          <i class="bi bi-calendar-event text-black"></i>
-          <p className="fw-bold d-inline-block ms-2 small">Eventi</p>
+          <i className="bi bi-calendar-event text-black"></i>
+          <span className="fw-bold d-inline-block ms-2 small">Eventi</span>
         </Card.Text>
       </Card.Body>
     </Card>
