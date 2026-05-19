20f67dbcbfca535714184bcc33ca768729be08c5
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-05-19 12:20:34 +0200

    fix upload foto

--- src/components/AddImages.jsx
+++ src/components/AddImages.jsx
@@ -1,30 +1,34 @@
-import { Form, Button } from "react-bootstrap";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { uploadProfilePictureAction } from "./path/verso/le/tue/actions";
+import { Form, Button } from "react-bootstrap"
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import AddImagesAction from "../redux/actions/images action/pictureProfilo"
 
 const AddImages = function () {
-  const [selectedPicture, setSelectedPicture] = useState(null);
-  const dispatch = useDispatch();
+  const [selectedPicture, setSelectedPicture] = useState(null)
+  const dispatch = useDispatch()
+
+  const profilo = useSelector((storeRedux) => {
+    return storeRedux.profile.me
+  })
 
   return (
     <>
       <Form.Control
         type="file"
         onChange={(e) => {
-          setSelectedPicture(e.target.files[0]);
+          setSelectedPicture(e.target.files[0])
         }}
       />
       <Button
-        disabled={!selectedPicture}
+        disabled={!selectedPicture || !profilo}
         onClick={() => {
-          dispatch(uploadProfilePictureAction(selectedPicture));
+          dispatch(AddImagesAction(selectedPicture, profilo._id))
         }}
       >
         Carica Foto
       </Button>
     </>
-  );
-};
+  )
+}
 
-export default AddImages;
+export default AddImages
---
+++ src/redux/actions/images action/pictureProfilo.js
@@ -0,0 +1,37 @@
+export const POST_PROFILE_PICTURES = "POST_PROFILE_PICTURES"
+
+const AddImagesAction = (file, userId) => {
+  return (dispatch) => {
+    const picture = new FormData()
+    picture.append("profile", file)
+
+    fetch(
+      `https://striveschool-api.herokuapp.com/api/profile/${userId}/picture`,
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2YTBhZDViOTA2YmJlOTAwMTVkZWU1N2YiLCJpYXQiOjE3NzkwOTQ5NjksImV4cCI6MTc4MDMwNDU2OX0.lCWAGVeHSActGSTjMyk8RMF3Ua0zXKkTnQcNrIuiP20",
+        },
+        body: picture,
+      },
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error("errore nella response")
+        }
+      })
+      .then((data) => {
+        dispatch({
+          type: POST_PROFILE_PICTURES,
+          payload: data,
+        })
+      })
+      .catch((error) => {
+        console.log("errore nella fetch", error)
+      })
+  }
+}
+export default AddImagesAction
