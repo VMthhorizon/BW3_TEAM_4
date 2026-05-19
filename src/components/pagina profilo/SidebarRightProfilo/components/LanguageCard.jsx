dad73fb712f3b9a8d136dfceec876bb1aa57fa5b
Vals098 <valeria.farinosi@gmail.com>
2026-05-19 13:08:18 +0200

    add: modal to language pencil

--- src/components/pagina profilo/SidebarRightProfilo/components/LanguageCard.jsx
+++ src/components/pagina profilo/SidebarRightProfilo/components/LanguageCard.jsx
@@ -1,24 +1,53 @@
 import { Pencil } from "react-bootstrap-icons"
+import { useState } from "react"
+import { Modal, Button } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
 
 const LanguageCard = function () {
+  const [showLanguageModal, setShowLanguageModal] = useState(false)
+  const navigate = useNavigate()
+
+
   return (
-    <div className="sidebar-card">
-      <div className="sidebar-card-content">
-        <div className="language-row">
-          <h5>Profile language</h5>
-          <Pencil className="pencil-icon" />
-        </div>
-        <p>Italiano</p>
+    <>
+      <div className="sidebar-card">
+        <div className="sidebar-card-content">
+          <div className="language-row">
+            <h5>Profile language</h5>
+            <Pencil className="pencil-icon" onClick={() => setShowLanguageModal(true)} />
+          </div>
+          <p>Italiano</p>
 
-        <hr />
+          <hr />
 
-        <div className="language-row">
-          <h5>Public profile & URL</h5>
-          <Pencil className="pencil-icon" />
+          <div className="language-row">
+            <h5>Public profile & URL</h5>
+            <Pencil className="pencil-icon" onClick={() => navigate("/settings")} />
+          </div>
+          <p>www.linkedin.com/in/name</p>
         </div>
-        <p>www.linkedin.com/in/name</p>
       </div>
-    </div>
+
+      {/* modale lingua*/}
+      <Modal show={showLanguageModal} onHide={() => setShowLanguageModal(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit language</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Form</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowLanguageModal(false)}>
+            Close
+          </Button>
+
+          <Button variant="primary">Save</Button>
+        </Modal.Footer>
+      </Modal>
+      
+    </>
   )
 }
 
