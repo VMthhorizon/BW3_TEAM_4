255e3044bde2dbd3038a19dc1dbf09297ad66671
Vals098 <valeria.farinosi@gmail.com>
2026-05-19 18:18:50 +0200

    update: PostCards

--- src/components/pagina home/PostCard.jsx
+++ src/components/pagina home/PostCard.jsx
@@ -1,14 +1,59 @@
-import { Card } from "react-bootstrap"
+import { HandThumbsUp, ChatText, Repeat, Send } from "react-bootstrap-icons"
+import linkedinAvatar from "../../assets/linkedin-avatar.png"
 
 const PostCard = function ({ post }) {
   return (
-    <Card className="mb-3">
-      <Card.Body>
-        <h6>{post.username}</h6>
+    <div className="sidebar-card mb-2">
+      <div className="sidebar-card-content">
+        {/* header */}
+        <div className="d-flex align-items-center gap-2 mb-3">
+          <img
+            src={linkedinAvatar}
+            alt="avatar"
+            className="round-avatar"
+            style={{
+              width: "48px",
+              height: "48px",
+              objectFit: "cover",
+            }}
+          />
 
-        <p>{post.text}</p>
-      </Card.Body>
-    </Card>
+          <div>
+            <h6 className="mb-0">{post.username}</h6>
+
+            <p className="text-muted mb-0">
+              {new Date(post.createdAt).toLocaleDateString()}
+            </p>
+          </div>
+        </div>
+
+        {/* testo */}
+        <p className="mb-3">{post.text}</p>
+
+        {/* footer */}
+        <div className="d-flex justify-content-around border-top pt-2">
+          <div className="post-action">
+            <HandThumbsUp />
+            <span>Consiglia</span>
+          </div>
+
+          <div className="post-action">
+            <ChatText />
+            <span>Commenta</span>
+          </div>
+
+          <div className="post-action">
+            <Repeat />
+            <span>Diffondi</span>
+          </div>
+
+          <div className="post-action">
+            <Send />
+            <span>Invia</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
