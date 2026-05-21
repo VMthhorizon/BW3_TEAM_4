e538338ce8955d242d4723a9a15c2df35a375338
AlessiaCotini <alessia.cotini@icloud.com>
2026-05-21 16:11:57 +0200

    text adjust

--- src/components/pagina notifiche/CentralNotifiche.jsx
+++ src/components/pagina notifiche/CentralNotifiche.jsx
@@ -31,30 +31,33 @@ const CentralNotifiche = function () {
     <>
       <Col xs={12}>
         <div className="p-3 bg-white border rounded-3 d-flex gap-2">
-          <Button variant="success" className="rounded-pill px-3 fw-bold">
+          <Button
+            variant="outline-success"
+            className="rounded-pill px-3 fw-bold"
+          >
             Tutto
           </Button>
           <Button
-            variant="outline-secondary"
+            variant="outline-success"
             className="rounded-pill px-3 fw-bold"
           >
             Lavoro
           </Button>
           <Button
-            variant="outline-secondary"
+            variant="outline-success"
             className="rounded-pill px-3 fw-bold"
           >
             I miei post
           </Button>
           <Button
-            variant="outline-secondary"
+            variant="outline-success"
             className="rounded-pill px-3 fw-bold"
           >
             Menzioni
           </Button>
         </div>
       </Col>
-      <Col xs={12} className="mt-3">
+      <Col xs={12} className="mt-2">
         <Card
           show={showNotifications}
           onHide={() => setShowNotifications(false)}
